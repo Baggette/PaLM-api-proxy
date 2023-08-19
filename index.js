e8280542e6bddf7ea2a1eef69cd3595bda8106a2
Baggette <rishis@live.wsd1.org>
2023-08-19 15:02:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { TextServiceClient } =
+  require("@google-ai/generativelanguage").v1beta2;
+
+const { GoogleAuth } = require("google-auth-library");
+dotenv.config();
+const app = express();
+
+app.get('/', async (req, res) => {
+    if(req.query.api_key == null || req.query.prompt == null) return res.send("Please provide an API key and prompt");
+    const MODEL_NAME = "models/text-bison-001";
+    const API_KEY = req.query.api_key;
+
+    const client = new TextServiceClient({
+        authClient: new GoogleAuth().fromAPIKey(API_KEY),
+    });
+
+    const prompt = `${req.query.prompt}`;
+
+    client
+        .generateText({
+        model: MODEL_NAME,
+        prompt: {
+          text: prompt,
+        },
+      })
+    .then((result) => {
+        console.log(JSON.stringify(result));
+        res.send(JSON.stringify(result));});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+})
