46eedfec1901c3a914589e8923798ac5d0220cee
iTsMaaT <matoulimar1@gmail.com>
2024-03-29 16:59:59 -0400

    added Gemini ability and full PaLM response parameter

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { TextServiceClient } =
-  require("@google-ai/generativelanguage").v1beta2;
+const express = require("express");
+const dotenv = require("dotenv");
 
+const { TextServiceClient } = require("@google-ai/generativelanguage").v1beta2;
 const { GoogleAuth } = require("google-auth-library");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 dotenv.config();
 const app = express();
 
-app.get('/', async (req, res) => {
-    if(req.query.api_key == null || req.query.prompt == null) return res.send("Please provide an API key and prompt");
-    const MODEL_NAME = "models/text-bison-001";
+app.get("/", async (req, res) => {
+    if (req.query.api_key == null || req.query.prompt == null) return res.send("Please provide an API key and prompt");
+    const PALM_MODEL_NAME = "models/text-bison-001";
     const API_KEY = req.query.api_key;
+    const PROMPT = `${req.query.prompt}`;
 
-    const client = new TextServiceClient({
-        authClient: new GoogleAuth().fromAPIKey(API_KEY),
-    });
+    if (!req.query.gemini) {
 
-    const prompt = `${req.query.prompt}`;
+        const client = new TextServiceClient({
+            authClient: new GoogleAuth().fromAPIKey(API_KEY),
+        });
 
-    client
-        .generateText({
-        model: MODEL_NAME,
-        prompt: {
-          text: prompt,
-        },
-      })
-    .then((result) => {
-        console.log(JSON.stringify(result[0].candidates[0].output));
-        res.send(`{"response" : ${JSON.stringify(result[0].candidates[0].output) || "Sorry I don't feel comfortable answering that question."}}`);
-})})
+        const result = await client.generateText({
+            model: PALM_MODEL_NAME,
+            prompt: {
+                text: PROMPT,
+            },
+        });
+
+        console.log(JSON.stringify(result));
+        res.send(req.query.full ? `{"response" : ${JSON.stringify(result)}}` : `{"response" : ${JSON.stringify(result[0].candidates[0].output) || "Sorry I don't feel comfortable answering that question."}}`);
+    } else {
+        const genAI = new GoogleGenerativeAI(API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const result = await model.generateContent(PROMPT).response;
+        const text = result.text();
+
+        console.log(text);
+        res.send(`{"response" : ${text || "Sorry I don't feel comfortable answering that question."}}`);
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
